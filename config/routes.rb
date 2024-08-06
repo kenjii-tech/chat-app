642c682ca5b54d1463e04715e705516bff133bb2
kenjii-tech <techskj61@gmail.com>
2024-08-06 16:46:39 +0900

    バリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
